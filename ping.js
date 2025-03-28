39feb070d4556b1f9f8c672649abd029c6cee916
SOUL KILLER OFC <153274881+Ayeshherath@users.noreply.github.com>
2025-03-28 09:56:02 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -11,10 +11,10 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
+        const message = await conn.sendMessage(from, { text: '> *Pinging...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *𝐒ᴜʟᴀ 𝐌ᴅ  𝐒ᴘᴇᴇᴅ : ${ping}ms 🍷*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> pong : ${ping}ms` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
