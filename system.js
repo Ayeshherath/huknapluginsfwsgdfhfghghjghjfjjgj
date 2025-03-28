b82111678f6dcf34f1d1fa9d4927de3c4379ac8f
SOUL KILLER OFC <153274881+Ayeshherath@users.noreply.github.com>
2025-03-28 09:54:41 +0530

    Update system.js

--- system.js
+++ system.js
@@ -12,17 +12,11 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `╭━━〔 *𝐒𝐔𝐋𝐀-𝐌𝐃* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• *⏳Uptime*: ${runtime(process.uptime())}
-┃◈┃• *📟 Ram*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-┃◈┃• *⚙️ Platform:-* ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: Sulaksha Madara 
-┃◈└───────────┈⊷
-╰──────────────┈⊷
+let status = `
+┃ *Uptime*: ${runtime(process.uptime())}
+┃ *Ram*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 
-> 🄿🄾🅆🄴🅁🄳 🅱🆈 𝐒𝐔𝐋𝐀_𝐌𝐃 😈
-`
+> 𝙼𝙰𝙽𝙰𝙶𝙴𝚁   𝙱𝙾𝚃™`
 return reply(`${status}`)
   
 }catch(e){
